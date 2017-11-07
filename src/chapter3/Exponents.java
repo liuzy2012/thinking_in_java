adbe6534722298397cdd4b6a2f497898b254dc55
Vincent <liuzhaoyang94@163.com>
2017-11-07 15:16:43 +0800

    thinking_in_java

---
+++ src/chapter3/Exponents.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class Exponents {
+	public static void main(String[] args) {
+		//大小写 'e' 是相同的
+		float expFloat=1.39e-43f;
+		expFloat=1.39E-43f;
+		System.out.println(expFloat);
+		double expDouble=47e47d;
+		double expDouble2=47e47;
+		System.out.println(expDouble);
+	}
+}
