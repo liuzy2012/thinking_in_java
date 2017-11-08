1539d2d266e1bd7b858969ee182a3a507daee4d5
Vincent <liuzhaoyang94@163.com>
2017-11-08 10:28:01 +0800

    exercise

---
+++ src/chapter5/Exercise2.java
@@ -0,0 +1,16 @@
+package chapter5;
+/*
+ * 
+ * 
+ * */
+public class Exercise2 {
+	String s1="test",s2;
+	public Exercise2(String s) {
+		s2=s;
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		Exercise2 ex2=new Exercise2("constructor");
+		System.out.println("s1="+ex2.s1+" ex2="+ex2.s2);
+	}
+}
