9453c5b0e1e04e028366f0fe67cbf97e15f1695c
Vincent <liuzhaoyang94@163.com>
2017-11-08 10:22:22 +0800

    exercise

--- src/chapter4/Exercise9.java
+++ src/chapter4/Exercise9.java
@@ -1,4 +1,4 @@
-package chapter3;
+package chapter4;
 
 public class Exercise9 {
 	public static void main(String[] args) {
---
+++ src/chapter5/Exercise1.java
@@ -0,0 +1,9 @@
+package chapter5;
+
+public class Exercise1 {
+	String s;
+	public static void main(String[] args) {
+		Exercise1 ex1=new Exercise1();
+		System.out.println(ex1.s);
+	}
+}
