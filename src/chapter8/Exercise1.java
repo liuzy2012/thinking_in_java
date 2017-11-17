8c690b62240ea224b8d34a4c15786998d2e96c1c
Vincent <liuzhaoyang94@163.com>
2017-11-17 13:58:13 +0800

    多态之向上转型

---
+++ src/chapter8/Exercise1.java
@@ -0,0 +1,22 @@
+package chapter8;
+
+class Cycle{
+	void ride(Cycle c) {
+		System.out.println("Cycle ride..."+c.toString());
+	}
+}
+class Unicycle extends Cycle{
+	
+}
+class Bycycle extends Cycle{
+	
+}
+class Tricycle extends Cycle{
+	
+}
+public class Exercise1 {
+	public static void main(String[] args) {
+		new Cycle().ride(new Unicycle());
+		new Cycle().ride(new Bycycle());
+	}
+}
